af7d7ccf5c03d2a33e39fc6eacce0171f38e6c8e
Natalia <sienkiewicz_n@wp.pl>
2019-10-20 01:22:45 +0200

    Pełny działający program. Poprawiono wykres matplotlib.

--- Lab1/Konwerter_walut.py
+++ Lab1/Konwerter_walut.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import requests
 
 currency1 = 'USD'
-currency2 = 'JPY'
+currency2 = 'EUR'
 date_from = '2018-01-01'
 date_to = '2018-01-30'
 
@@ -18,26 +18,21 @@ def fetch_currency(currency,beg,end):
 rate1 = fetch_currency(currency1,date_from,date_to)
 rate2 = fetch_currency(currency2,date_from,date_to)
 
-usd_rates_january = pd.DataFrame.from_dict(rate1)
+usd_rates_january = pd.DataFrame.from_dict(rate1).head(10)
 usd_rates_january.head()
 
-jpy_rates_january = pd.DataFrame.from_dict(rate2)
+jpy_rates_january = pd.DataFrame.from_dict(rate2).head(10)
 jpy_rates_january.head()
 
 plot_data1 = usd_rates_january.set_index(['effectiveDate'])['mid']
 plot_data2 = jpy_rates_january.set_index(['effectiveDate'])['mid']
 
-plt.plot(plot_data1)
-plt.show()
-
-plt.plot(plot_data2)
-plt.show()
-
-
-print(rate1)
-print(rate2)
-
 correlation = np.corrcoef (plot_data1, plot_data2)[0][1]
 
-
-print("{} {}".format("Corelation: ", correlation))
+plt.plot(plot_data1, 'g--', plot_data2,'b--')
+plt.ylim(ymin=0)
+plt.title('Korelacja {} do {} = {}'.format(currency1, currency2, correlation))
+plt.ylabel('Wartość w PLN')
+plt.xlabel('Data')
+plt.legend([currency1, currency2], loc='lower right')
+plt.show()
