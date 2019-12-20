e344c7e3245e4371fa063463f340432f91715cd8
Natalia <sienkiewicz_n@wp.pl>
2019-12-20 19:17:31 +0100

    Ukończenie Lab3

--- Lab3/Zad1.py
+++ Lab3/Zad1.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -28,14 +29,14 @@ sns.pairplot(boston_df)
 plt.show()
 
 # Dużo zmiennych... na początek spróbujmy z jedną
-l_pokoi = boston_nieruchomosci['data'][:, np.newaxis, 5]
-plt.scatter(l_pokoi, boston_nieruchomosci['target'])
+przestepczosc = boston_nieruchomosci['data'][:, np.newaxis, 0]
+plt.scatter(przestepczosc, boston_nieruchomosci['target'])
 plt.show()
 
 # Stworzenie regresora liniowego
 linreg = LinearRegression()
 
-X_train, X_test, y_train, y_test = train_test_split(l_pokoi, boston_nieruchomosci['target'], test_size = 0.3)
+X_train, X_test, y_train, y_test = train_test_split(przestepczosc, boston_nieruchomosci['target'], test_size = 0.3)
 
 linreg.fit(X_train, y_train)
 
@@ -46,7 +47,6 @@ y_pred = linreg.predict(X_test)
 print('Metryka domyślna: ', linreg.score(X_test, y_test))
 
 # wskaźnik (metryka) r^2
-# dokumentacja: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.r2_score.html#sklearn.metrics.r2_score
 print('Metryka r2: ', r2_score(y_test, y_pred))
 
 # współczynniki regresji
@@ -57,18 +57,13 @@ plt.scatter(X_test, y_test, color='black')
 plt.plot(X_test, y_pred, color='blue', linewidth=2)
 plt.show()
 
-# dokumentacja: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.cross_val_score.html
-from sklearn.model_selection import cross_val_score
-
-cv_score_r2 = cross_val_score(linreg, l_pokoi, boston_nieruchomosci.target, cv=5, scoring='r2')
+cv_score_r2 = cross_val_score(linreg, przestepczosc, boston_nieruchomosci.target, cv=5, scoring='r2')
 print(cv_score_r2)
 
-# używamy innej metryki
-# dokumentacja:
-# https://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter
-cv_score_ev = cross_val_score(linreg, l_pokoi, boston_nieruchomosci.target, cv=5, scoring='explained_variance')
+# użycie innej metryki
+cv_score_ev = cross_val_score(linreg, przestepczosc, boston_nieruchomosci.target, cv=5, scoring='explained_variance')
 print(cv_score_ev)
 
 # ...i jeszcze innej
-cv_score_mse = cross_val_score(linreg, l_pokoi, boston_nieruchomosci.target, cv=5, scoring='neg_mean_squared_error')
+cv_score_mse = cross_val_score(linreg, przestepczosc, boston_nieruchomosci.target, cv=5, scoring='neg_mean_squared_error')
 print(cv_score_mse)
